326162a8c81ac741b1d3e65f3bb83e2dfa48b471
Dwinlight <corentinpouget15@gmail.com>
2019-02-09 21:01:01 +0100

    creation des fichiers

---
+++ index.js
@@ -0,0 +1,12 @@
+var i = 0;
+//console.log(i);
+//var elt = document.getElementsByClassName('a')[0];
+var elt = document.querySelector("#titre");
+//var elt2 = document.getElementsByClassName('ajout');
+//console.log(elt2);
+//var elt = document.getElementsByClassName('ajout')[0];
+console.log(elt.valueOf());
+//elt.addEventListener('click', function() {
+//                     i++;
+//                     document.createElement("p").textContent(""+ i+1);
+//                     });
